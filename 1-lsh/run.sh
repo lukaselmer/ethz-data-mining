5e24e19138a5b8cc1d8503f8a56dcaa7b2212106
Sandro Felicioni <sandro.felicioni@gmail.com>
2014-03-09 11:18:23 +0100

    added missing sort

--- 1-lsh/run.sh
+++ 1-lsh/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # generate prediction file
-cat 1_data/train.txt | ./mapper.py | ./reducer.py > 1_data/prediction.txt
+cat 1_data/train.txt | ./mapper.py | sort | ./reducer.py > 1_data/prediction.txt
 
 # run check.py for seeing results
 ./check.py 1_data/prediction.txt 1_data/duplicates.txt 
