b4f83526c0c4001b16afaaf1d47e3bb501510c37
Lukas Elmer <lukas.elmer@gmail.com>
2014-03-11 11:55:30 +0100

    F1 Score = 1.0?

--- 1-lsh/run.sh
+++ 1-lsh/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # generate prediction file
-cat 1_data/train.txt | ./src/mapper.py 200 43 | sort | ./reducer.py > 1_data/prediction.txt
+cat 1_data/train.txt | ./src/mapper.py 165 35 | sort | ./src/reducer.py > 1_data/prediction.txt
 # run check.py for seeing results
 ./check.py 1_data/prediction.txt 1_data/duplicates.txt 
