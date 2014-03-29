a8806d0e7d17e252ee2cbccf06c4419f77d1c036
Lukas Elmer <lukas.elmer@gmail.com>
2014-03-29 14:28:46 +0100

    evaluate

--- 2-svm/code/run.sh
+++ 2-svm/code/run.sh
@@ -6,4 +6,4 @@ cat ./1-data/training | ./mapper.py | ./reducer.py > 1-data/weights.txt
 
 
 # run check.py for seeing results
-./evaluate.py 1-data/weights.txt 1-data/test_data.txt 1_data/test_labels.txt
+./evaluate.py 1-data/weights.txt 1-data/test_data.txt 1-data/test_labels.txt
