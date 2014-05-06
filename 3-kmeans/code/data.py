c7646643a240d0a91908d4af532ef5ebc6f9b1fe
Lukas Elmer <lukas.elmer@gmail.com>
2014-05-06 14:02:53 +0200

    some basics...

--- 3-kmeans/code/data.py
+++ 3-kmeans/code/data.py
@@ -1,12 +1,27 @@
 #!/usr/bin/env python2.7
 
 import numpy as np
+import datetime
+
+c = datetime.datetime.now()
+print(c)
+
+print 'Loading npz...'
 
 # Load the file stored in NPZ format
-data = np.load("training.npz")['arr_0']
+data = np.load("1-data/training.npz")['arr_0']
+
+print 'Saving csv...'
 
 # To store the file in CSV format
-np.savetxt('training.csv', data)
+#np.savetxt('1-data/training.csv', data)
+
+print 'Loading csv...'
 
 # Load CSV file
-data = np.loadtxt('training.csv')
+#data = np.loadtxt('1-data/training.csv')
+
+print '...done!'
+
+print(datetime.datetime.now())
+print(datetime.datetime.now() - c)
