b002e80f28ec707e1fc833de1ac73e3af78c2e1d
Lukas Elmer <lukas.elmer@gmail.com>
2014-05-08 03:10:23 +0200

    minibatch reducer

--- 3-kmeans/code/run.py
+++ 3-kmeans/code/run.py
@@ -1,27 +1,38 @@
 #!/usr/bin/env python2.7
 
 import sys
-from sklearn.cluster import k_means
+from sklearn.cluster import k_means, MiniBatchKMeans, KMeans
 import numpy as np
 import datetime
 
 
 if __name__ == "__main__":
-    arr = []
+    #arr = []
+    #arr.append(np.fromstring(line, dtype=np.float, sep=' '))
+    #print np.array(arr)
 
-    arr.append(np.fromstring(line, dtype=np.float, sep=' '))
-
-    print np.array(arr)
-
-    #c = datetime.datetime.now()
-    #print(c)
-    #data = np.load("1-data/training.npz")['arr_0']
+    c = datetime.datetime.now()
+    print(c)
+    data = np.load("1-data/training.npz")['arr_0']
     #data = data[:60000]
-    #print('Data loaded')
+    print('Data loaded')
+
+    m = MiniBatchKMeans(n_clusters=200, n_init=1, batch_size=200000, verbose=1)#, batch_size=6000) #, init_size=1000
+    m.fit(data)
+
+    print("Fitted!")
+
+    centers = m.cluster_centers_
+    score = m.score(data) / len(data) * -1
+
+    print("Score: %f" % score)
+
+    print('Saving results')
+    np.savetxt('1-data/centers.csv', centers)
+
     #res = k_means(data, n_clusters=200)
-    #print('Saving results')
     #np.savetxt('1-data/run_result.csv', res[0])
     #print(res)
-    #print(datetime.datetime.now())
-    #print(datetime.datetime.now() - c)
+    print(datetime.datetime.now())
+    print(datetime.datetime.now() - c)
     pass
