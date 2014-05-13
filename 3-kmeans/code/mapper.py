95d5b30ee8a1e9cd809c0d703504d112373e327b
Lukas Elmer <lukas.elmer@gmail.com>
2014-05-13 21:36:56 +0200

    3832

--- 3-kmeans/code/mapper.py
+++ 3-kmeans/code/mapper.py
@@ -90,6 +90,7 @@ class DataPoint():
         return 1.0 / self.calc_sampling_weight() / out_per_mapper
 
 
+
 class Mapper:
     def __init__(self):
         total_rows_in_reducer, mappers = [100000, 15] if "--local" in sys.argv else [75000*5, 300]
