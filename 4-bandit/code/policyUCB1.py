eaefcc0ea89c9ce5eda00106d786f2e4867cf2a2
Sandro Felicioni <sandro.felicioni@gmail.com>
2014-05-25 15:31:29 +0200

    Fixed some bugs in UCB along with some optimizations. Score on yahoo data:   CTR=0.045333 and 1114 seconds

--- 4-bandit/code/policyUCB1.py
+++ 4-bandit/code/policyUCB1.py
@@ -9,7 +9,7 @@ np.random.seed(42)
 articlesDict = None
 bandits = {}        # dictionary of type <articleNr, (meanReward, nrTrials)>
 currentRecommendation = None
-n = 0
+n = 1
 
 # Evaluator will call this function and pass the article features.
 # Check evaluator.py description for details.
@@ -22,13 +22,16 @@ def set_articles(art):
 # Check task description for details.
 def update(reward):
     global bandits
-    normalizedReward = (reward + 1) / np.float(2)
+    global n
 
-    # update meanReward and nrTrials
-    meanReward, nrTrial = bandits[currentRecommendation]
-    nrTrial += 1
-    meanReward += 1/np.float(nrTrial) * (normalizedReward - meanReward)
-    bandits[currentRecommendation] = (meanReward, nrTrial)
+    if reward != -1:  # just update model when reward is 0 or 1 (when we know what happened)
+        n += 1
+
+        # update meanReward and nrTrials
+        meanReward, nrTrial = bandits[currentRecommendation]
+        nrTrial += 1
+        meanReward += 1/np.float(nrTrial) * (reward - meanReward)
+        bandits[currentRecommendation] = (meanReward, nrTrial)
 
 # compute ucb for a given article
 def ucb(article, n):
@@ -43,24 +46,20 @@ def reccomend(timestamp, user_features, articles):
     global bandits
     global n
 
-    # count number of rounds
-    n += 1
-
     # initialization: recommend each machine once
     for article in np.random.permutation(articles):
-        if not bandits.has_key(article):
+        if not bandits.has_key(article) or bandits[article][1] == 0:
             bandits[article] = (0, 0)
             currentRecommendation = article
-            return article
+            return currentRecommendation
 
     # play machine j which maximizes upper confidence bound of x_j
     bestUCB = -np.inf
-    bestArticle = None
     for article in articles:
         currentUCB = ucb(article, n)
 
         if currentUCB > bestUCB:
             bestUCB = currentUCB
-            bestArticle = article
+            currentRecommendation = article
 
-    return bestArticle
+    return currentRecommendation
