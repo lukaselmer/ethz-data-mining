3fb3b0656f0196e66eb5053f8155fb7508b8764d
Frederick Egli <frederick.egli@gmail.com>
2014-05-25 14:17:24 +0200

    linucb

--- 4-bandit/code/policyLinUCB.py
+++ 4-bandit/code/policyLinUCB.py
@@ -7,9 +7,13 @@ import math
 all_articles = []
 newArticles = {}
 all_M={}
+all_inverseCache={}
 all_b={}
 all_w = {}
 alpha=0.835
+lastArticle=[]
+lastUser=[]
+totalLine=0;
 
 
 # This function initializes all articles as new
@@ -30,11 +34,18 @@ def set_articles(art):
 # This function will be called by the evaluator.
 # Check task description for details.
 def update(reward):
-    pass
+    if reward == -1:
+        pass
+    else:
+        all_M[lastArticle] = all_M[lastArticle]+np.dot(lastUser,np.transpose(lastUser))
+        if reward == 1:
+            all_b[lastArticle] = all_b[lastArticle]+lastUser
+
 
 # This function will be called by the evaluator.
 # Check task description for details.
 def reccomend(timestamp, user_features, articles):
+    user_features = np.transpose(np.mat(user_features))
     articlesWithMaxUCB = []
     bestUCB = 0;
 
@@ -43,12 +54,20 @@ def reccomend(timestamp, user_features, articles):
             newArticles[article] = False
             all_M[article] = np.identity(6)  #'todo change 6 to size....'
             all_b[article] = np.zeros((6, 1)) #'todo change 6 to size....'
-        inverse=np.linalg.inv(all_M[article])
+        if totalLine%1000 == 0 or not all_inverseCache.has_key(article):
+            #print 'drin at %d' %(totalLine)
+            all_inverseCache[article] = np.linalg.inv(all_M[article])
+        inverse=all_inverseCache[article]
         all_w[article] = np.dot(inverse,all_b[article])
         ucb_current = np.dot(np.transpose(all_w[article]), user_features)[0] + alpha*math.sqrt(np.dot(np.dot(np.transpose(user_features),inverse),user_features))
         if(ucb_current>=bestUCB):
             bestUCB=ucb_current
             articlesWithMaxUCB.append(article)
-
-    return random.choice(articlesWithMaxUCB) #if several articles had the same UCB value
+    global totalLine
+    totalLine+=1
+    global lastUser
+    lastUser = user_features
+    global lastArticle
+    lastArticle=random.choice(articlesWithMaxUCB) #if several articles had the same UCB value
+    return lastArticle
 
