d5778c038f52caa1dc4e6b45c126cdbea28f361d
Sandro Felicioni <sandro.felicioni@gmail.com>
2014-05-27 21:46:17 +0200

    Added code to run either on just one day or on the entire dataset (10 days)

--- 4-bandit/code/collect_yahoo_articles.py
+++ 4-bandit/code/collect_yahoo_articles.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python2.7
 import sys
+import gzip
 
 # This file was used to extract the articles (with features) from the yahoo log files. The output was saved to
 # articles_yahoo.txt.
@@ -8,29 +9,30 @@ def process(path):
 
     articles = {}
 
-    with file(path) as inf:
+    for i in range(10):
+        with gzip.open(path % (i+1), 'rb') as inf:
 
-        for line in inf:
-            # Parsing the log line.
-            logline = line.strip().split()
+            for line in inf:
+                # Parsing the log line.
+                logline = line.strip().split()
 
-            article_id = 0
-            for feat in logline[10:]:
+                article_id = 0
+                for feat in logline[10:]:
 
-                if feat[0] == '|':
-                    if article_id != 0 and len(articles[article_id]) < 6:
-                        # print 'error at article %i: %s' % (article_id, line)
-                        del articles[article_id]
+                    if feat[0] == '|':
+                        if article_id != 0 and len(articles[article_id]) < 6:
+                            # print 'error at article %i: %s' % (article_id, line)
+                            del articles[article_id]
 
-                    article_id = int(feat[1:])
-                    if article_id in articles:
-                        article_id = 0
-                    else:
-                        #if article_id != 109528:
-                        #    print '%i. article %i found' % (len(articles) + 1 , article_id)
-                        articles[article_id] = {}
-                elif article_id != 0:
-                    articles[article_id][int(feat[0])-1] = float(feat[2:])
+                        article_id = int(feat[1:])
+                        if article_id in articles:
+                            article_id = 0
+                        else:
+                            #if article_id != 109528:
+                            #    print '%i. article %i found' % (len(articles) + 1 , article_id)
+                            articles[article_id] = {}
+                    elif article_id != 0:
+                        articles[article_id][int(feat[0])-1] = float(feat[2:])
 
     for art_id, feats in articles.iteritems():
         if len(feats) == 6:
--- 4-bandit/code/run_yahoo.sh
+++ 4-bandit/code/run_yahoo.sh
@@ -2,7 +2,7 @@
 
 echo "start recommending..."
 START=$(date +%s)
-./evaluator_yahoo.py ../data/articles_yahoo.txt ../data/log_yahoo_subset.gz
+./evaluator_yahoo.py ../data/articles_yahoo.txt ../data/log_yahoo.gz
 END=$(date +%s)
 DIFF=$(( $END - $START ))
 echo "It took $DIFF seconds"
