041957bf12e9bbaf55647402dd694355fb3786d7
Lukas Elmer <lukas.elmer@gmail.com>
2014-06-04 13:49:52 +0200

    logging

--- 4-bandit/code/hideSomePolicyLinUCB.py
+++ 4-bandit/code/hideSomePolicyLinUCB.py
@@ -23,7 +23,7 @@ class FiFoQueue():
             return 0
         return float(sum(self.arr)) / len(self.arr)
 
-    def check_avoid(self, k, article_stats):
+    def check_avoidasdfasdfasdfa(self, k, article_stats):
         a = article_stats[k][0]
         b = article_stats[k][1]
         ratio = 1.0
@@ -64,20 +64,21 @@ class HideSomeLinUCB:
     all_b = {}
     all_w = {}
     all_avoid_queues = {}
-    ucb_stats = {}
+    #ucb_stats = {}
     alpha = 0.2
     count = 0
     stats_count = 0
     current_article = None  # current recommendation
     current_user = None  # user for which the article was recommended
-    ucb_stats = {'lines_evaluated': 0, 'clicked': 0}
-    article_stats = {}
+    #ucb_stats = {'lines_evaluated': 0, 'clicked': 0}
+    #article_stats = {}
 
     def set_articles(self, articles):
         print 'We are using an alpha of: %f \n' % (self.alpha)
         print 'NORMAL LINUCB VERSION!!!!!\n'
         self.all_articles = articles
 
+        self.bad_log = {}
         # initialize M and b for each article:
         for article in self.all_articles:
             M = np.identity(6)
@@ -88,9 +89,9 @@ class HideSomeLinUCB:
             self.all_b[article] = b
             self.all_M_inv[article] = M_inv
             self.all_w[article] = np.dot(M_inv, b)
-            self.all_avoid_queues[article] = FiFoQueue()
-            self.article_stats[article] = {2: 0, 0: 0, 1: 0}
-
+            #self.all_avoid_queues[article] = FiFoQueue()
+            #self.article_stats[article] = {2: 0, 0: 0, 1: 0}
+            #self.bad_log[article] = {}
 
     def ucb(self, article, user):
         M_inv = self.all_M_inv[article]
@@ -109,62 +110,71 @@ class HideSomeLinUCB:
         self.stats_count += 1
         if self.stats_count > 500001:
             self.stats_count = 1
-            ucb = float(self.ucb_stats['clicked']) / self.ucb_stats['lines_evaluated']
-            print "Timestamp: %i, UCB: %f, Lines Evaluated: %f" % (timestamp, ucb, self.ucb_stats['lines_evaluated'])
-            for k in self.article_stats:
-                a = self.article_stats[k][0]
-                b = self.article_stats[k][1]
-                if a + b > 0:
-                    self.article_stats[k][3] = float(b) / (a + b)
+            #ucb = float(self.ucb_stats['clicked']) / self.ucb_stats['lines_evaluated']
+            #print "Timestamp: %i, UCB: %f, Lines Evaluated: %f" % (timestamp, ucb, self.ucb_stats['lines_evaluated'])
+            #for k in self.article_stats:
+            #    a = self.article_stats[k][0]
+            #    b = self.article_stats[k][1]
+            #    if a + b > 0:
+            #        self.article_stats[k][3] = float(b) / (a + b)
             #print self.article_stats
 
         if self.count > 10000:
             self.count = 0
 
-            for k in self.all_avoid_queues:
-                q = self.all_avoid_queues[k]
-                if q.check_avoid(k, self.article_stats):
-                    print "Avoiding article"
-                    print k
+            #for k in self.all_avoid_queues:
+            #    q = self.all_avoid_queues[k]
+            #    if q.check_avoid(k, self.article_stats):
+            #        print "Avoiding article"
+            #        print k
 
                     # print ", ".join([str([x.good_ratio(), x.max_good_ratio]) for x in self.all_avoid_queues.values()])
 
+        best_article = self.current_article
         best_ucb = -np.inf
-        for article in articles:
-            if self.all_avoid_queues[article].should_avoid():
-                continue
+        for article in self.filter_bad(articles):
+            #if self.all_avoid_queues[article].should_avoid():
+            #    continue
 
             current_ucb = self.ucb(article, user_features)
 
-            if current_ucb > best_ucb:
+            if current_ucb >= best_ucb:
                 best_ucb = current_ucb
-                self.current_article = article
+                best_article = article
+
+        self.current_article = best_article
 
         self.current_user = user_features
         return self.current_article
 
+    def filter_bad(self, articles):
+        if self.bad_log.has_key(self.count):
+            filter_article = self.bad_log[self.count]
+            articles = filter(articles, lambda a: filter_article is not a)
+        return articles
+
+    def do_update(self, reward, article):
+        #self.ucb_stats['lines_evaluated'] += 1
+        #self.article_stats[article][reward] += 1
+        #self.ucb_stats['clicked'] += reward
+        user = self.current_user
+        M = self.all_M[article]
+        b = self.all_b[article]
+        self.all_M[article] = M + np.dot(user, user.T)
+        self.all_b[article] = b + reward * user
+        # precompute M^-1 and w for UCB
+        self.all_M_inv[article] = np.linalg.inv(self.all_M[article])
+        self.all_w[article] = np.dot(self.all_M_inv[article], self.all_b[article])
+        #self.all_avoid_queues[article].enqueue(reward)
 
     def update(self, reward):
-        self.article_stats[self.current_article][2] += 1
+        #self.article_stats[self.current_article][2] += 1
 
         if reward == 0 or reward == 1:
-            self.ucb_stats['lines_evaluated'] += 1
-            self.article_stats[self.current_article][reward] += 1
-            self.ucb_stats['clicked'] += reward
-
-            article = self.current_article
-            user = self.current_user
-            M = self.all_M[article]
-            b = self.all_b[article]
-
-            self.all_M[article] = M + np.dot(user, user.T)
-            self.all_b[article] = b + reward * user
-
-            # precompute M^-1 and w for UCB
-            self.all_M_inv[article] = np.linalg.inv(self.all_M[article])
-            self.all_w[article] = np.dot(self.all_M_inv[article], self.all_b[article])
-
-            self.all_avoid_queues[article].enqueue(reward)
+            if reward == 0:
+                # TODO: self.bad_log[self.count] = self.current_article
+                print "%i %i" % (self.count, self.current_article)
+            self.do_update(reward, self.current_article)
 
 
 linucb = HideSomeLinUCB()
